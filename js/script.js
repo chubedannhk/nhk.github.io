3381ef1e4f902e4b916b8f9753d929b03d6c172d
chubedannhk <108327470+chubedannhk@users.noreply.github.com>
2023-05-22 13:43:40 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// Thêm chức năng tự động chuyển đổi ảnh sau 10 giây
+setInterval(function() {
+  plusSlides(1);
+}, 10000);
+
+// back to top
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document with smooth scrolling
+function topFunction() {
+  // Scroll smoothly to the top of the document
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
